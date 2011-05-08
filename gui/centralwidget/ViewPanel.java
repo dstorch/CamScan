1a1d3f9f6b95729ac61c191d2d0467452628fa1b
stelios <sanagnos@cs.brown.edu>
2011-05-07 20:35:32 -0400

    scrollbars for view mode (can't center img though)

--- gui/centralwidget/ViewPanel.java
+++ gui/centralwidget/ViewPanel.java
@@ -1,149 +1,18 @@
-//package centralwidget;
-//
-//import java.awt.Color;
-//import java.awt.Dimension;
-//import java.awt.Graphics;
-//import java.awt.Graphics2D;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.MouseWheelEvent;
-//import java.awt.event.MouseWheelListener;
-//import java.awt.image.BufferedImage;
-//
-//import javax.swing.ImageIcon;
-//import javax.swing.JLabel;
-//import javax.swing.JPanel;
-//import javax.swing.JScrollPane;
-//import javax.swing.border.Border;
-//import javax.swing.border.LineBorder;
-//
-//import core.Parameters;
-//import java.awt.event.ActionListener;
-//
-///**
-// * This panel is used for only viewing a page.
-// * 
-// * @author Stelios
-// *
-// */
-//public class ViewPanel extends JPanel implements MouseWheelListener {
-//	
-//	/****************************************
-//	 * 
-//	 * Private Instance Variables
-//	 * 
-//	 ****************************************/
-//	
-//	/**
-//	 * The buffered image representing the page.
-//	 */
-//	private BufferedImage img;
-//	
-//	/**
-//	 * The scale factor.
-//	 */
-//	private double scaleFactor;
-//	
-//	private ImageIcon imageIcon;
-//	
-//	/****************************************
-//	 * 
-//	 * Constructor(s)
-//	 * 
-//	 ****************************************/
-//	
-//	/**
-//	 * Constructor.
-//	 */
-//	public ViewPanel() {
-//		super();
-//		this.setBackground(Color.LIGHT_GRAY);
-//		this.addMouseWheelListener(this);
-//		this.setBorder(new LineBorder(Color.GRAY));
-////		this.imageIcon = new ImageIcon(this.img);
-////		JLabel imageLabel = new JLabel(this.imageIcon);
-////		this.add(imageLabel);
-//		this.scaleFactor = 1;
-//	}
-//	
-//	/****************************************
-//	 * 
-//	 * Public Methods
-//	 * 
-//	 ****************************************/
-//	
-//	/**
-//	 * The paintComponent method.
-//	 */
-//	@Override
-//	public void paintComponent(Graphics g) {
-//		super.paintComponent(g);
-//		
-//		if (this.img != Parameters.getCoreManager().getWorkingImage()) {
-//			this.scaleFactor = 1;
-//			this.img = Parameters.getCoreManager().getWorkingImage();
-//			
-//			double xSideRatio = ((double) this.getWidth())/((double) this.img.getWidth());
-//			double ySideRatio = ((double) this.getHeight())/((double) this.img.getHeight());
-//			
-//			if (ySideRatio > xSideRatio)
-//				this.scaleFactor = xSideRatio;
-//			else
-//				this.scaleFactor = ySideRatio;
-//			
-//			this.scaleFactor *= 0.95;
-//		}
-//		
-//		if (this.img != null) {
-//			int newW = (int) (this.img.getWidth() * this.scaleFactor);
-//			int newH = (int) (this.img.getHeight() * this.scaleFactor);
-//			g.drawImage(this.img, (this.getWidth() - newW)/2, (this.getHeight() - newH)/2, newW, newH, null);
-//		}
-//		
-////		int w = (int) (this.getWidth()*this.scaleFactor);
-////		int h = (int) (this.getHeight()*this.scaleFactor);
-////		System.out.println(w + " " + h);
-////		this.setSize(w, h);
-//	}
-//
-//	/**
-//	 * Handles the mouse wheel movement.
-//	 * Used for zooming.
-//	 */
-//	public void mouseWheelMoved(MouseWheelEvent e) {
-//
-//		int notches = e.getWheelRotation();
-//
-//		if (notches < 0) {
-//			if (this.scaleFactor > 0.2)
-//				this.scaleFactor -= 0.2;
-//		} else {
-//			this.scaleFactor += 0.2;
-//		}
-//		
-//		this.repaint();
-//	}
-//        
-//}
-
-
 package centralwidget;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 import core.Parameters;
-import java.awt.event.ActionListener;
 
 /**
  * This panel is used for only viewing a page.
@@ -151,48 +20,55 @@ import java.awt.event.ActionListener;
  * @author Stelios
  *
  */
-public class ViewPanel extends JPanel implements MouseWheelListener {
-	
+public class ViewPanel extends JPanel {
+
 	/****************************************
 	 * 
 	 * Private Instance Variables
 	 * 
 	 ****************************************/
-	
+
 	/**
 	 * The buffered image representing the page.
 	 */
 	private BufferedImage img;
-	
+
+	/**
+	 * The image label holding the page image.
+	 */
+	private JLabel imageLabel;
+
 	/**
 	 * The scale factor.
 	 */
 	private double scaleFactor;
-	
+
 	/****************************************
 	 * 
 	 * Constructor(s)
 	 * 
 	 ****************************************/
-	
+
 	/**
 	 * Constructor.
 	 */
 	public ViewPanel() {
 		super();
+		this.setLayout(new BorderLayout());
 		this.setBackground(Color.LIGHT_GRAY);
-		this.addMouseWheelListener(this);
 		this.setBorder(new LineBorder(Color.GRAY));
-		
 		this.scaleFactor = 1;
+		this.imageLabel = new JLabel();
+		JScrollPane imageScroller = new JScrollPane(this.imageLabel);
+		this.add(imageScroller, BorderLayout.CENTER);
 	}
-	
+
 	/****************************************
 	 * 
 	 * Public Methods
 	 * 
 	 ****************************************/
-	
+
 	/**
 	 * Zooms in.
 	 */
@@ -200,66 +76,68 @@ public class ViewPanel extends JPanel implements MouseWheelListener {
 		this.scaleFactor += 0.2;
 		this.repaint();
 	}
-	
+
 	/**
 	 * Zooms out.
 	 */
 	public void zoomOut() {
-		if (this.scaleFactor > 0.2)
+		if (this.scaleFactor > 0.2) {
 			this.scaleFactor -= 0.2;
-		this.repaint();
+			this.repaint();
+		}
 	}
-	
+
 	/**
 	 * The paintComponent method.
 	 */
 	@Override
 	public void paintComponent(Graphics g) {
+		
 		super.paintComponent(g);
 		
-		if (this.img != Parameters.getCoreManager().getWorkingImage()) {
-			this.scaleFactor = 1;
+		// When the page has changed, set the new img instance variable
+		// and modify the scale factor.
+		if (this.img != Parameters.getCoreManager().getWorkingImage() && Parameters.getCoreManager().getWorkingImage() != null) {
+
 			this.img = Parameters.getCoreManager().getWorkingImage();
-			
-			double xSideRatio = ((double) this.getWidth())/((double) this.img.getWidth());
-			double ySideRatio = ((double) this.getHeight())/((double) this.img.getHeight());
-			
+
+			double xSideRatio = ((double) this.getWidth())/((double) this.imageLabel.getWidth());
+			double ySideRatio = ((double) this.getHeight())/((double) this.imageLabel.getHeight());
+
 			if (ySideRatio > xSideRatio)
 				this.scaleFactor = xSideRatio;
 			else
 				this.scaleFactor = ySideRatio;
-			
+
 			this.scaleFactor *= 0.95;
 		}
 		
+		// Scale and draw the image.
 		if (this.img != null) {
-			int newW = (int) (this.img.getWidth() * this.scaleFactor);
-			int newH = (int) (this.img.getHeight() * this.scaleFactor);
-			g.drawImage(this.img, (this.getWidth() - newW)/2, (this.getHeight() - newH)/2, newW, newH, null);
+			int newW = (int) (this.img.getWidth(this) * this.scaleFactor);
+			int newH = (int) (this.img.getHeight(this) * this.scaleFactor);
+			this.imageLabel.setLocation((this.getWidth() - newW)/2, (this.getHeight() - newH)/2);
+			this.imageLabel.setBounds((this.getWidth() - newW)/2, (this.getHeight() - newH)/2, newW, newH);
+			this.imageLabel.setIcon(this.getScaledImageIcon());
 		}
-		
-//		int w = (int) (this.getWidth()*this.scaleFactor);
-//		int h = (int) (this.getHeight()*this.scaleFactor);
-//		System.out.println(w + " " + h);
-//		this.setSize(w, h);
 	}
 
 	/**
-	 * Handles the mouse wheel movement.
-	 * Used for zooming.
+	 * Scales the given buffered image and returns in as an
+	 * ImageIcon.
+	 * 
+	 * @param src The buffered image to scale
+	 * @param scale The scale factor
+	 * @return The resulting ImageIcon
 	 */
-	public void mouseWheelMoved(MouseWheelEvent e) {
-
-		int notches = e.getWheelRotation();
-
-		if (notches < 0) {
-			if (this.scaleFactor > 0.2)
-				this.scaleFactor -= 0.1;
-		} else {
-			this.scaleFactor += 0.1;
-		}
-		
-		this.repaint();
+	private ImageIcon getScaledImageIcon() {
+		int w = (int)(this.scaleFactor * this.img.getWidth(this));
+		int h = (int)(this.scaleFactor * this.img.getHeight(this));
+		int type = BufferedImage.TYPE_INT_RGB;
+		BufferedImage dst = new BufferedImage(w, h, type);
+		Graphics2D g2 = dst.createGraphics();
+		g2.drawImage(this.img, 0, 0, w, h, this.imageLabel);
+		g2.dispose();
+		return new ImageIcon(dst);
 	}
-        
 }
